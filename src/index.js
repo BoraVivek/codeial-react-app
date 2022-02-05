1fe0ab16290cb999ef577726d069a4e803326c19
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2022-02-05 09:51:01 +0530

    Form Hooks Part 1 & Toast Notifications Implementation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import { App } from "./components";
 
+import { Toaster } from 'react-hot-toast'; //React Notification Package
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Toaster position='top-left' /> {/* Calling the Toaster component here, so that it works gloablly throughout our application */}
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import styles from "../styles/login.module.css";
+
 const Login = () => {
-    return <h1>Login</h1>
+
+    // Creating States for email, password, and logging in
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loggingIn, setLoggingIn] = useState(false);
+
+    // Handles the form submission 
+    const handleSubmit = (e) => {
+        // Preventing default behavior of form
+        e.preventDefault();
+        //Setting the loggingin state to true, so that the button gets disabled.
+        setLoggingIn(true);
+
+        // If email or password is not provided, then show a toast notification
+        if(!email || !password){
+            return toast.error("Please enter both email and password");
+        }
+    }
+
+    return (
+        // On Submitting the form calling handleSubmit function
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
+            <span className={styles.loginSignupHeader}>Log In</span>
+
+            <div className={styles.field}>
+                <input type="email" name="email" id="email" placeholder="Email"  
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} />
+            </div>
+
+            <div className={styles.field}>
+                <input type="password" name="password" id="password" placeholder="Password"  
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} />
+            </div>
+
+            <div className={styles.field}>
+                {/* Disable Button if the form is processing */}
+                <button disabled={loggingIn}>
+                    {/* Show Loggingin message, so that user can know that the form is processing. */}
+                    {loggingIn ? 'Logging In...' : 'Login'}
+                </button>
+            </div>
+        </form>
+    );
 }
 
 export default Login;
