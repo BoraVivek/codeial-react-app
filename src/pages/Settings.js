7c0cc64b54b5d8d13a21b8d9795a95e7a1987ba8
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2022-02-11 08:56:56 +0530

    Settings Logic & Conditional Rendering

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,25 @@
 import styles from "../styles/settings.module.css";
 import { useAuth } from "../hooks";
+import { useState } from "react";
 
 const Settings = () => {
+
+    // get authentication details
     const auth = useAuth();
 
-    return(
+    // States
+    const [name, setName] = useState(auth.user?.name ? auth.user.name : '');
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [savingForm, setSavingForm] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    //Function to update profile
+    const updateProfile = () => {
+        setSavingForm(true);
+    }
+
+    return (
         <div className={styles.settings}>
             <div className={styles.imgContainer}>
                 <img src="https://www.svgrepo.com/show/255192/profile.svg" alt="" />
@@ -19,29 +34,65 @@ const Settings = () => {
                     {auth.user?.email}
                 </div>
             </div>
-            <div className={styles.field}>
-                <div className={styles.fieldLabel}>
-                    Name
+            {/* If editing Mode is enabled show the input box, otherwise show the name text. */}
+            {isEditing ?
+                <div className={styles.field}>
+                    <div className={styles.fieldLabel}>
+                        Name
+                    </div>
+                    <div className={styles.fieldValue}>
+                        <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
                 </div>
-                <div className={styles.fieldValue}>
-                    {auth.user?.name}
+                :
+                <div className={styles.field}>
+                    <div className={styles.fieldLabel}>
+                        Name
+                    </div>
+                    <div className={styles.fieldValue}>
+                        {auth.user?.name}
+                    </div>
                 </div>
-            </div>
-            <div className={styles.field}>
-                <div className={styles.fieldLabel}>
-                    Password
-                </div>
-                <input type="password" name="password" id="password" />
-            </div>
-            <div className={styles.field}>
-                <div className={styles.fieldLabel}>
-                    Confirm Password
-                </div>
-                <input type="password" name="confirm_password" id="confirmPassword" />
-            </div>
+            }
 
+            {/* Show input boxes only if editing mode is enabled */}
+            {isEditing &&
+                <>
+                    <div className={styles.field}>
+                        <div className={styles.fieldLabel}>
+                            Password
+                        </div>
+                        <input type="password" name="password" id="password" value={password} onClick={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className={styles.field}>
+                        <div className={styles.fieldLabel}>
+                            Confirm Password
+                        </div>
+                        <input type="password" name="confirm_password" id="confirmPassword" value={confirmPassword} onClick={(e) => setConfirmPassword(e.target.value)} />
+                    </div>
+                </>
+            }
+
+            {/* Buttons */}
             <div className={styles.btnGrp}>
-                <button className={`button ${styles.editBtn}`}>Edit Profile</button>  
+                {/* Show Save and Go Back button if editing mode is enabled, else show Edit Profile button which enabled Editing Mode */}
+                {isEditing ? 
+                    <>
+                        {/* Calls the updateProfile function */}
+                        <button className={`button ${styles.saveBtn}`} disabled={savingForm} onClick={updateProfile}>
+                            {savingForm ? 'Saving profile...' : 'Save Profile'}
+                        </button>
+
+                        {/* Disables the editing mode */}
+                        <button className={`button ${styles.goBack}`} onClick={() => setIsEditing(false)}>Go Back</button>
+                    </>
+                    :
+                    // Enables the editing Mode
+                    <button className={`button ${styles.editBtn}`} onClick={() => setIsEditing(true)}>
+                        Edit Profile
+                    </button>
+                }
+                
             </div>
         </div>
     );
